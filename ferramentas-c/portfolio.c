6b265efb8b6331854091a799b18ff59b7ba340a4
João Marcos Araújo Lima <jmarcosaraujo7@gmail.com>
2025-10-06 21:41:54 -0300

    Adiciona portfolio.c (C)

---
+++ ferramentas-c/portfolio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("===== Portfólio =====\n");
+    printf("Nome: João Lima\n");
+    printf("Profissão: Desenvolvedor C\n");
+    printf("Projetos:\n");
+    printf("1. Calculadora\n");
+    printf("2. Calendário\n");
+    printf("3. Relógio\n");
+    return 0;
+}
